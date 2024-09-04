10bdbf7ffbc66df9a154bc5d3e2c45674f8468ce
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-09-04 17:39:15 +0100

    React intro

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,45 @@
+// task_5/dashboard/config/webpack.config.js
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
+        }),
+    ],
+    devtool: 'inline-source-map',
+};
